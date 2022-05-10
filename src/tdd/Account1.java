f235c1caf8e500231756704420b0785bc3f4d91a
UZOCHUKWU CHINEMEREM <chat.with.ucjhay@gmail.com>
2022-05-10 13:05:24 +0100

    Test file

---
+++ src/tdd/Account1.java
@@ -0,0 +1,16 @@
+package tdd;
+
+public class Account1 {
+//instance variable
+    private String name;
+
+//set name in the object and store name method
+    public void setName(String name) {
+        this.name = name;
+    }
+
+//get name and return name to caller method
+    public String getName() {
+        return name;
+    }
+}
